b7aed5782a0b12859ab656e3b81cd95f268c5197
AidaGube <aidagube@gmail.com>
2021-11-25 22:38:22 +0300

    sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,28 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('header_active');
-  })
+function sidebarMenu(selector) {
+	let menu = $(selector);
+	let button = menu.find('.sidebar-menu__button');
+	let links = menu.find('.menu__link');
+	let overlay = menu.find('.sidebar-menu__overlay');
+	
+	button.on('click', (e) => {
+		e.preventDefault();
+		toggleMenu();
+	});
+	
+	links.on('click',() => toggleMenu());
+	overlay.on('click',() => toggleMenu());
+
+
+	function toggleMenu(){
+		menu.toggleClass('sidebar-menu_active');
+		
+		if (menu.hasClass('sidebar-menu_active')) {
+			$('body').css('overflow', 'hidden');
+		} else {
+			$('body').css('overflow', 'visible');
+		}
+	}
+}
+
+
+burgerMenu ('.burger-menu');
