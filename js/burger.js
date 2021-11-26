8c7f4f67f7dbd2fbb9b23df943139b18853d9634
AidaGube <aidagube@gmail.com>
2021-11-26 17:41:46 +0300

    button

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,9 @@
-$(document).ready (function() {
-$ ('.header__burger').click(function(event) {
-	$ ('.header__burger, .header__menu').toggleClass('active');
-
-});
-});
+const iconMenu = document.querySelector('.header__burger');
+if (iconMenu) {
+	const menuHeader = document.querySelector('.header__menu');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('header__burger_active');
+	menuHeader.classList.toggle('header__menu_active');
+	});
+}
