6caba51400a79aacea2d029b7cd5e88a3f0a5ad8
AidaGube <aidagube@gmail.com>
2021-11-29 17:48:34 +0300

    i

--- js/burger.js
+++ js/burger.js
@@ -3,7 +3,7 @@ if (iconMenu) {
 	const menuHeader = document.querySelector('.header__menu');
 	iconMenu.addEventListener("click", function(e) {
 		document.body.classList.toggle('_lock');
-    iconMenu.classList.toggle('header__burger_active');
-	menuHeader.classList.toggle('header__menu_active');
+    iconMenu.classList.toggle('_active');
+	menuHeader.classList.toggle('_active');
 	});
 }
